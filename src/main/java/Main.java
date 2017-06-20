2a8715bd9799c47aa3817a8648bb5a788a647d5b
Andrew Lau <andrew.lau@ausnimbus.com.au>
2017-06-20 12:09:51 +1000

    Initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,27 @@
+import ratpack.exec.Blocking;
+import ratpack.server.BaseDir;
+import ratpack.server.RatpackServer;
+import ratpack.groovy.template.TextTemplateModule;
+import ratpack.guice.Guice;
+
+import static ratpack.groovy.Groovy.groovyTemplate;
+
+import java.util.*;
+import java.sql.*;
+
+public class Main {
+  public static void main(String... args) throws Exception {
+    RatpackServer.start(s -> s
+        .serverConfig(c -> c
+          .baseDir(BaseDir.find())
+          .env())
+
+        .registry(Guice.registry(b -> b
+          .module(TextTemplateModule.class, conf -> conf.setStaticallyCompile(true))))
+
+        .handlers(chain -> chain
+            .get(ctx -> ctx.render(groovyTemplate("index.html")))
+        )
+    );
+  }
+}
